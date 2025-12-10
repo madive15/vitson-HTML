4920c61cbdd620bd1f8e93619389a53be130a5ce
lee dong girl <0193100266@naver.com>
2025-12-10 15:03:17 +0900

    feat: Organize scss directory structure

--- src/app.js
+++ src/app.js
@@ -1,5 +1,9 @@
 import '@/assets/scripts';
-import '@/assets/scss/main.scss';
+import '@/assets/scss/vendors/index.scss';
+import '@/assets/scss/base/index.scss';
+import '@/assets/scss/layout/index.scss';
+import '@/assets/scss/components/index.scss';
+import '@/assets/scss/pages/index.scss';
 
 console.log(`%c ==== ${APP_ENV_ROOT}.${APP_ENV_TYPE} run ====`, 'color: green');
 console.log('%c APP_ENV_URL :', 'color: green', APP_ENV_URL);
