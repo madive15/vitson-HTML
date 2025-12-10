9013263c8c6a9ee24e04225f85e8df6bc7dc8001
lee dong girl <0193100266@naver.com>
2025-12-10 16:42:48 +0900

    fix: update code

--- src/assets/scripts/ui/test.ts
+++ src/assets/scripts/ui/test.ts
@@ -1,3 +1,52 @@
+function createChart() {
+  $('#chart').kendoChart({
+    title: {
+      text: 'Site Visitors Stats'
+    },
+    subtitle: {
+      text: '/thousands/'
+    },
+    legend: {
+      visible: false
+    },
+    seriesDefaults: {
+      type: 'bar'
+    },
+    series: [
+      {
+        name: 'Total Visits',
+        data: [56000, 63000, 74000, 91000, 117000, 138000]
+      },
+      {
+        name: 'Unique visitors',
+        data: [52000, 34000, 23000, 48000, 67000, 83000]
+      }
+    ],
+    valueAxis: {
+      max: 140000,
+      line: {
+        visible: false
+      },
+      minorGridLines: {
+        visible: true
+      },
+      labels: {
+        rotation: 'auto'
+      }
+    },
+    categoryAxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+      majorGridLines: {
+        visible: false
+      }
+    },
+    tooltip: {
+      visible: true,
+      template: '#= series.name #: #= value #'
+    }
+  });
+}
+
 $(document).ready(function () {
   // Initialize Kendo Buttons
   if ($('#kendoButton').length) {
@@ -54,4 +103,8 @@ $(document).ready(function () {
       value: 'Phone (SMS)'
     });
   }
+
+  createChart();
 });
+
+$(document).bind('kendo:skinChange', createChart);
