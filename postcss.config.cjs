fde5ff8c84de07c3c47f052a427178b045bb2247
lee dong girl <oww1220@gmail.com>
2026-01-02 16:13:39 +0900

    feat: update css 주석제거 로직 추가

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,8 +1,13 @@
 /** @type {import('postcss-load-config').Config} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const config = {
   plugins: {
     // '@tailwindcss/postcss': {},
-    autoprefixer: {}
+    autoprefixer: {},
+    ...(isProd && {
+      'postcss-discard-comments': {removeAll: true}
+    })
   }
 };
 
