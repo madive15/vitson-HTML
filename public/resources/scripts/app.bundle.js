d639491daba1f45841eeaad817cfa927220161bf
oww1220 <oww1220@users.noreply.github.com>
2026-01-07 05:01:24 +0000

    deploy: 3aeb64389a3f789193024842d1a25dc8826b3da6

---
+++ public/resources/scripts/app.bundle.js
@@ -0,0 +1,373 @@
+/******/ (function() { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 370:
+/***/ (function(__unused_webpack_module, __unused_webpack___webpack_exports__, __webpack_require__) {
+
+"use strict";
+
+// EXTERNAL MODULE: ./node_modules/.pnpm/swiper@11.2.10/node_modules/swiper/swiper.mjs + 1 modules
+var swiper = __webpack_require__(723);
+// EXTERNAL MODULE: ./node_modules/.pnpm/swiper@11.2.10/node_modules/swiper/modules/index.mjs + 27 modules
+var modules = __webpack_require__(951);
+;// ./src/assets/scripts/ui/swiper.ts
+/* eslint-disable @typescript-eslint/no-unused-vars */
+/* eslint-disable no-unused-vars */
+
+
+window.addEventListener('DOMContentLoaded', event => {
+  const swiper1 = new swiper/* default */.A('.swiper-page-nav', {
+    modules: [modules/* Navigation */.Vx, modules/* Pagination */.dK],
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction'
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  });
+  const swiper2 = new swiper/* default */.A('.swiper-page', {
+    modules: [modules/* Pagination */.dK],
+    pagination: {
+      el: '.swiper-pagination'
+    }
+  });
+});
+// EXTERNAL MODULE: ./src/assets/scripts/ui/test.ts
+var test = __webpack_require__(445);
+;// ./src/assets/scripts/index.ts
+
+
+;// ./src/app.js
+
+
+
+
+
+
+
+console.log(`%c ==== ${"app"}.${"js"} run ====`, 'color: green');
+console.log('%c APP_ENV_URL :', 'color: green', "pc");
+console.log('%c APP_ENV_TYPE :', 'color: green', "js");
+console.log('%c ====================', 'color: green');
+
+/***/ }),
+
+/***/ 445:
+/***/ (function() {
+
+/*
+function createChart() {
+  $('#chart').kendoChart({
+    title: {
+      text: 'Site Visitors Stats'
+    },
+    subtitle: {
+      text: '/thousands/'
+    },
+    legend: {
+      visible: false
+    },
+    seriesDefaults: {
+      type: 'bar'
+    },
+    series: [
+      {
+        name: 'Total Visits',
+        data: [56000, 63000, 74000, 91000, 117000, 138000]
+      },
+      {name: 'Unique visitors', data: [52000, 34000, 23000, 48000, 67000, 83000]}
+    ],
+    valueAxis: {
+      max: 140000,
+      line: {
+        visible: false
+      },
+      minorGridLines: {
+        visible: true
+      },
+      labels: {
+        rotation: 'auto'
+      }
+    },
+    categoryAxis: {
+      categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+      majorGridLines: {
+        visible: false
+      }
+    },
+    tooltip: {
+      visible: true,
+      template: '#= series.name #: #= value #'
+    }
+  });
+}
+*/
+
+$(document).ready(function () {
+  // Initialize Kendo Buttons
+  if ($('#kendoButton').length) {
+    $('#kendoButton').kendoButton({
+      themeColor: 'primary',
+      enable: true
+    });
+    $('#kendoPrimaryButton').kendoButton({
+      icon: 'filter',
+      size: 'large',
+      click: e => {
+        console.log(e);
+      }
+    });
+    $('#kendoLargeButton').kendoButton({
+      rounded: 'full',
+      // none | small | medium | large | full
+      fillMode: 'solid',
+      // solid | outline | flat | link
+      themeColor: 'primary' // base | primary | secondary | success | etc
+    });
+    $('#customSizedButton').kendoButton({
+      size: 'small'
+    });
+  }
+
+  // Templated Button
+  if ($('#templatedButtonContainer').length) {
+    const buttonTemplate = kendo.template(`<button id='#= id #' type='button' class='k-button k-button-lg'><span class='k-icon k-i-#= icon #'></span> #: text #</button>`);
+    const buttonData = {
+      id: 'myTemplatedButton',
+      text: 'Templated Button',
+      icon: 'save'
+    };
+    $('#templatedButtonContainer').html(buttonTemplate(buttonData));
+    $('#myTemplatedButton').kendoButton({
+      click: () => {
+        alert('Templated button clicked!');
+      }
+    });
+  }
+
+  // Kendo UI RadioButtons are typically styled via CSS classes ('k-radio', 'k-radio-label')
+
+  if ($('#engine1').length) {
+    // Add existence check for radio buttons
+    $('#engine1').kendoRadioButton({
+      label: `<span class="k-radio-label-text">1.4 Petrol, 92kW</span><span class="k-radio-label-description">A útis consummationem.</span>`,
+      checked: true,
+      encoded: false
+    });
+    $('#engine2').kendoRadioButton({
+      label: '1.8 Petrol, 118kW'
+    });
+    $('#engine3').kendoRadioButton({
+      label: '2.0 Petrol, 147kW',
+      enabled: false
+    });
+  }
+  if ($('#radiogroup').length) {
+    // Add existence check for radio group
+    const radioItems = [{
+      label: 'Phone (SMS)',
+      value: 'phone',
+      description: 'Receive notifications via SMS'
+    }, {
+      label: 'E-mail',
+      value: 'email',
+      description: 'Receive notifications via E-mail'
+    }, {
+      label: 'None',
+      value: 'none',
+      description: 'Do not receive any notifications'
+    }];
+    const radioTemplate = kendo.template(`
+      <li>
+        <input type="radio" name="notification" id="radio-#: value #" class="k-radio" value="#: value #" />
+        <label for="radio-#: value #" class="k-radio-label">
+          <span class="k-radio-label-text">#: label #</span>
+          <span class="k-radio-label-description">#: description #</span>
+        </label>
+      </li>
+    `);
+    const radioGroupElement = $('#radiogroup');
+    radioItems.forEach(item => {
+      radioGroupElement.append(radioTemplate(item));
+    });
+    radioGroupElement.kendoRadioGroup({
+      layout: 'horizontal',
+      value: 'phone'
+    });
+  }
+  if ($('#radiogroup2').length) {
+    $('#radiogroup2').kendoRadioGroup({
+      items: [{
+        label: 'Phone (SMS)',
+        value: 'phone'
+      }, {
+        label: 'E-mail',
+        value: 'email'
+      }, {
+        label: 'None',
+        value: 'none'
+      }],
+      layout: 'vertical',
+      value: 'email'
+    });
+  }
+
+  // createChart();
+});
+
+// $(document).bind('kendo:skinChange', createChart);
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = __webpack_modules__;
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/chunk loaded */
+/******/ 	!function() {
+/******/ 		var deferred = [];
+/******/ 		__webpack_require__.O = function(result, chunkIds, fn, priority) {
+/******/ 			if(chunkIds) {
+/******/ 				priority = priority || 0;
+/******/ 				for(var i = deferred.length; i > 0 && deferred[i - 1][2] > priority; i--) deferred[i] = deferred[i - 1];
+/******/ 				deferred[i] = [chunkIds, fn, priority];
+/******/ 				return;
+/******/ 			}
+/******/ 			var notFulfilled = Infinity;
+/******/ 			for (var i = 0; i < deferred.length; i++) {
+/******/ 				var chunkIds = deferred[i][0];
+/******/ 				var fn = deferred[i][1];
+/******/ 				var priority = deferred[i][2];
+/******/ 				var fulfilled = true;
+/******/ 				for (var j = 0; j < chunkIds.length; j++) {
+/******/ 					if ((priority & 1 === 0 || notFulfilled >= priority) && Object.keys(__webpack_require__.O).every(function(key) { return __webpack_require__.O[key](chunkIds[j]); })) {
+/******/ 						chunkIds.splice(j--, 1);
+/******/ 					} else {
+/******/ 						fulfilled = false;
+/******/ 						if(priority < notFulfilled) notFulfilled = priority;
+/******/ 					}
+/******/ 				}
+/******/ 				if(fulfilled) {
+/******/ 					deferred.splice(i--, 1)
+/******/ 					var r = fn();
+/******/ 					if (r !== undefined) result = r;
+/******/ 				}
+/******/ 			}
+/******/ 			return result;
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	!function() {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = function(exports, definition) {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	!function() {
+/******/ 		__webpack_require__.o = function(obj, prop) { return Object.prototype.hasOwnProperty.call(obj, prop); }
+/******/ 	}();
+/******/ 	
+/******/ 	/* webpack/runtime/jsonp chunk loading */
+/******/ 	!function() {
+/******/ 		// no baseURI
+/******/ 		
+/******/ 		// object to store loaded and loading chunks
+/******/ 		// undefined = chunk not loaded, null = chunk preloaded/prefetched
+/******/ 		// [resolve, reject, Promise] = chunk loading, 0 = chunk loaded
+/******/ 		var installedChunks = {
+/******/ 			524: 0,
+/******/ 			96: 0,
+/******/ 			152: 0,
+/******/ 			133: 0,
+/******/ 			237: 0
+/******/ 		};
+/******/ 		
+/******/ 		// no chunk on demand loading
+/******/ 		
+/******/ 		// no prefetching
+/******/ 		
+/******/ 		// no preloaded
+/******/ 		
+/******/ 		// no HMR
+/******/ 		
+/******/ 		// no HMR manifest
+/******/ 		
+/******/ 		__webpack_require__.O.j = function(chunkId) { return installedChunks[chunkId] === 0; };
+/******/ 		
+/******/ 		// install a JSONP callback for chunk loading
+/******/ 		var webpackJsonpCallback = function(parentChunkLoadingFunction, data) {
+/******/ 			var chunkIds = data[0];
+/******/ 			var moreModules = data[1];
+/******/ 			var runtime = data[2];
+/******/ 			// add "moreModules" to the modules object,
+/******/ 			// then flag all "chunkIds" as loaded and fire callback
+/******/ 			var moduleId, chunkId, i = 0;
+/******/ 			if(chunkIds.some(function(id) { return installedChunks[id] !== 0; })) {
+/******/ 				for(moduleId in moreModules) {
+/******/ 					if(__webpack_require__.o(moreModules, moduleId)) {
+/******/ 						__webpack_require__.m[moduleId] = moreModules[moduleId];
+/******/ 					}
+/******/ 				}
+/******/ 				if(runtime) var result = runtime(__webpack_require__);
+/******/ 			}
+/******/ 			if(parentChunkLoadingFunction) parentChunkLoadingFunction(data);
+/******/ 			for(;i < chunkIds.length; i++) {
+/******/ 				chunkId = chunkIds[i];
+/******/ 				if(__webpack_require__.o(installedChunks, chunkId) && installedChunks[chunkId]) {
+/******/ 					installedChunks[chunkId][0]();
+/******/ 				}
+/******/ 				installedChunks[chunkId] = 0;
+/******/ 			}
+/******/ 			return __webpack_require__.O(result);
+/******/ 		}
+/******/ 		
+/******/ 		var chunkLoadingGlobal = self["webpackChunkroot"] = self["webpackChunkroot"] || [];
+/******/ 		chunkLoadingGlobal.forEach(webpackJsonpCallback.bind(null, 0));
+/******/ 		chunkLoadingGlobal.push = webpackJsonpCallback.bind(null, chunkLoadingGlobal.push.bind(chunkLoadingGlobal));
+/******/ 	}();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module depends on other loaded chunks and execution need to be delayed
+/******/ 	var __webpack_exports__ = __webpack_require__.O(undefined, [96,152,133,237,979], function() { return __webpack_require__(370); })
+/******/ 	__webpack_exports__ = __webpack_require__.O(__webpack_exports__);
+/******/ 	
+/******/ })()
+;
